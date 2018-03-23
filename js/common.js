2f6a9d51f8b657f6266d05d1786eedc0938ef08f
ilya <jokerfokus@mail.ru>
2018-03-23 21:26:32 +0500

    git add .

--- js/common.js
+++ js/common.js
@@ -5,4 +5,30 @@ $(function() {
 		$(".hidden-mnu").fadeToggle();
 	});
 
+	$(".ask").on("click", function () {
+		$(this).next().slideToggle(300);
+		$(".que").not($(this).next()).slideUp(300); //.css("display", "none");
+		
+	});
+
+	$(".owl-work").owlCarousel({
+		items: 3,
+		loop: true,
+		dotsEach: true,
+		nav: true,
+		margin: 20,
+		navText: ['<i class="fa fa-angle-left fa-2x" aria-hidden="true"></i>', '<i class="fa fa-angle-right fa-2x" aria-hidden="true"></i>'],
+		responsive: {
+			0: {
+				items: 1
+			},
+			500: {
+				items: 2
+			},
+			1000: {
+				items: 3
+			}
+		}
+	});
+
 });
